27bdbb1a0169fdfa307fb481bf1fe7ae1fb74f09
Acuda <fmw-be@ipa.fhg.de>
2014-11-10 13:32:01 +0100

    publish x-sin test

--- move_sin.py
+++ move_sin.py
@@ -6,20 +6,36 @@ from geometry_msgs.msg import Twist
 import numpy as np
 import time
 
+
+x = np.linspace(0, np.pi * 2, 200)
+xsin = np.sin(x)
+maxvel = 0.1
+xsin *= maxvel
+
+xTwist = Twist()
+
+rospy.init_node('VID_TEST')
+pub = rospy.Publisher('/base_controller/command', Twist)
+
+for vel in xsin:
+    xTwist.linear.x = vel
+    pub.publish(xTwist)
+    time.sleep(0.01)
+
+'''
 stopTwist = Twist()
 print stopTwist
-xTwist = Twist()
-xTwist.linear.x = 0.1
 print xTwist
 
 rospy.init_node('VID_TEST')
 pub = rospy.Publisher('/base_controller/command', Twist)
+
 
 time.sleep(1)
 pub.publish(xTwist)
 time.sleep(1)
 pub.publish(stopTwist)
 
-
+'''
 #while not rospy.is_shutdown():
 #    pass
