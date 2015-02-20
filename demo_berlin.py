9f2ebc42d6c4d593755809f63b29ad1e2824bfab
Acuda <eistel@gmail.com>
2015-02-20 18:54:43 +0100

    -

--- demo_berlin.py
+++ demo_berlin.py
@@ -422,9 +422,23 @@ class DemoScene(BaseScene):
 
     def led_color_demo(self, steptime=1):
         self.syncTimeline()
+
         self.appendLed(r=1, g=0, b=0, a=1, frequency=0, mode=1)
         fillData = [None]*self.calc_samples(steptime)
         self.LED.extend(fillData)
+
+        self.appendLed(r=0, g=1, b=0, a=1, frequency=0, mode=1)
+        fillData = [None]*self.calc_samples(steptime)
+        self.LED.extend(fillData)
+
+        self.appendLed(r=0, g=0, b=1, a=1, frequency=0, mode=1)
+        fillData = [None]*self.calc_samples(steptime)
+        self.LED.extend(fillData)
+
+        self.appendLed()
+        fillData = [None]*self.calc_samples(steptime)
+        self.LED.extend(fillData)
+
         self.syncTimeline()
 
 
